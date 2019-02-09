14a79f346838a4f953ba56bde69a8b306020627b
Jonathan Shaw <jonshaw199@gmail.com>
2019-02-09 08:27:43 -0800

    masonry testing

---
+++ scripts/init-graphics.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	var folder = "images/";
+
+	$.ajax({
+		url : folder,
+		success: function (data) {
+			$(data).find("a").attr("href", function (i, val) {
+				if( val.match(/\.(jpe?g|png|gif)$/) ) { 
+					$("#tile-grid").append( "<div class=\"grid-item\"><img src='\"+ folder + val +\"'></div>" );
+				} 
+			});
+		}
+	});
+	
+	var $grid = $('.grid').imagesLoaded( function() {
+	  // init Masonry after all images have loaded
+	  $grid.masonry({
+		// options
+		  itemSelector: '.grid-item',
+		  columnWidth: 200,
+		  fitWidth: true
+	  });
+	});
+});
