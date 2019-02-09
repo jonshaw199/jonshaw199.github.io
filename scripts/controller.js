13d72241726396411a36c612bcff1b7be7da9800
Jonathan Shaw <jonshaw199@gmail.com>
2019-02-08 19:05:26 -0800

    Mobile tile animation fix

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,89 +1,10 @@
-var profs_db = TAFFY(proficiencies);
-var temp_db = TAFFY();
-
-/************************************************************************************************/
 $(document).ready(function(){
-	fillInPersonalStatement();
-	fillInSkills();
-	fillInEducation();
-	fillInWorkHistory();
-	$(".live-tile").liveTile();
+	$('.dropdown').on('show.bs.dropdown', function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+	});
+
+	// Add slideUp animation to Bootstrap dropdown when collapsing.
+	$('.dropdown').on('hide.bs.dropdown', function() {
+		$(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+	});
 });
-/************************************************************************************************/
-
-function fillInPersonalStatement() {
-	$('.personal-statement').text(personalStatement);
-}
-
-function fillInEducation() {
-	$('.sac-state-title').text(sacState.title);
-	$('.sac-state-date-range').text(sacState.dateRange);
-	$('.sac-state-degree').text(sacState.degree);
-	$('.sac-state-summary').text(sacState.summary);
-	$('.sierra-title').text(sierra.title);
-	$('.sierra-date-range').text(sierra.dateRange);
-	$('.sierra-degree').text(sierra.degree);
-	$('.sierra-summary').text(sierra.summary);
-}
-
-function fillInWorkHistory() {
-	$('.fpc-title').text(fpc.title);
-	$('.fpc-date-range').text(fpc.dateRange);
-	$('.fpc-company').text(fpc.company);
-	$('.fpc-summary').text(fpc.summary);
-	$('.irt-title').text(irt.title);
-	$('.irt-date-range').text(irt.dateRange);
-	$('.irt-company').text(irt.company);
-	$('.irt-summary').text(irt.summary);
-}
-
-function fillInSkills() {
-	populateTempDb('general');
-	updateProfs();
-}
-
-function populateTempDb(c1, c2, c3, c4, c5) {
-    temp_db().remove();
-    profs_db({tags:{has: [c1,c2, c3, c4, c5]}}).each(function (r) {
-        temp_db.insert(r);
-    });
-    temp_db.sort("level");
-}
-
-function updateProfs() {
-    var i = 12;
-    temp_db().each(function (r) {
-        temp_db(r).update({id:i});
-        i--;
-    });
-
-    var profCounter;
-    for(profCounter = 1; profCounter < 13; profCounter++) {
-        var title = "prof-" + profCounter + "-title";
-        var level = "prof-" + profCounter + "-shape";
-        var desc = "prof-" + profCounter + "-description";
-        var titles = document.getElementsByClassName(title);
-
-        var i;
-        for (i = 0; i < titles.length; i++) {
-            titles[i].innerHTML = temp_db({id:profCounter}).first().title;
-        }
-        var progressBarShapes = document.getElementsByClassName(level);
-        var j = temp_db({id:profCounter}).first().level;
-        for (i = 0; i < j; i++) {
-            progressBarShapes[i].style.backgroundColor = "#ffffff";
-        }
-        for(i = 10; i < j + 10; i++) {
-            progressBarShapes[i].style.backgroundColor = "#ffffff";
-        }
-        document.getElementById(desc).innerHTML = temp_db({id:profCounter}).first().description;
-    }
-}
-
-function toggle() {
-    if ($("#skills-more-less").attr('aria-expanded') === "true") {
-        $("#skills-more-less").text('Show More');
-    } else {
-        $("#skills-more-less").text('Show Less');
-    }
-}
--- scripts/init-home.js
+++ scripts/init-home.js
@@ -1,30 +1,39 @@
 $(document).ready(function(){
-  // Add smooth scrolling to all links
-  $(".smooth").on('click', function(event) {
+	window.addEventListener('scroll', loseFocus());
+	$('.smooth').on('click', function() {
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+			// Store hash
+			var hash = this.hash;
 
-      // Store hash
-      var hash = this.hash;
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - $('#nav').height()
+			}, 800, function(){
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top - $('#nav').height()
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-  
-  window.addEventListener("scroll", loseFocus());
-  
-  function loseFocus() {
-	$('.flip-card:focus').blur();
-  }
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+			});
+		} // End if
+	});
+	var flipCards = document.querySelectorAll(".flip-card");
+
+	for (var i = 0; i < flipCards.length; i++) {
+		flipCards[i].addEventListener('touchstart', function() {
+			//this.style.webkitTransform = 'rotate('+180+'deg)'; 
+			//this.style.mozTransform    = 'rotate('+180+'deg)'; 
+			//this.style.msTransform     = 'rotate('+180+'deg)'; 
+			//this.style.oTransform      = 'rotate('+180+'deg)'; 
+			//this.style.transform       = 'rotate('+180+'deg)';
+			$(this).toggleClass("hovered");
+		}, false);
+	}
 });
+
+function loseFocus() {
+	$('.hovered').blur();
+}
