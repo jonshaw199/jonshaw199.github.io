f8c411acb552c61c11e071d930662c0fdafc7cca
Jonathan Shaw <jonshaw199@gmail.com>
2019-05-20 13:46:03 -0700

    Fixing smooth scrolling

--- scripts/init-home.js
+++ scripts/init-home.js
@@ -10,11 +10,11 @@ $(document).ready(function(){
 
 				// Store hash
 				var hash = this.hash;
-
+				
 				// Using jQuery's animate() method to add smooth page scroll
 				// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 				$('html, body').animate({
-					scrollTop: $(hash).offset().top - $('nav').height()
+					scrollTop: $(hash).offset().top
 				}, 800, function(){
 
 				// Add hash (#) to URL when done scrolling (default click behavior)
