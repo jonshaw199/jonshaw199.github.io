7a9f332f8c5e9b232848657b9b09ffc74ea82f05
Jon <jonshaw199@gmail.com>
2021-04-18 11:02:24 -0700

    Removing everything but resume and simplifying

--- scripts/database.js
+++ scripts/database.js
@@ -1,128 +1,42 @@
-/*
-web_dev
-app_dev
-data_science
-back_end
-front_end
-design
-general
-*/
-
-var oldPersonalStatement = "I am a passionate and hard-working designer and developer with a uniquely diverse skill set. I am seeking a challenging position or project to apply my knowledge and improve upon my skills.";
-
-var oldPersonalStatement2 = "If you are looking for an experienced and multifaceted designer and/or developer then look no further! My name is Jonathan Shaw and I possess a uniquely diverse skill set, a passion for the field, and a willingness to learn."
-
-var areasOfFocusIntro = "Just some of the ways I can serve you include:"
-
-var areasOfFocusOld = [
-	{
-		title: "Web Apps",
-		description: "I'm experienced with developing full-stack web applications using such technologies as MongoDB, Express.js, React, Node.js, Redux, Reactstrap, HTML5, CSS3, and Bootstrap.",
-		keywords: ["MongoDB", "Express.js", "React", "strap", "Node.js", "Redux", "Reactstrap", "HTML5", "CSS3", "Bootstrap"]
-	},
-	{
-		title: "Desktop Apps",
-		description: "I'm extremely proficient with C, C++, and Java programming, and I can comfortably use them to develop complex and large-scale systems.",
-		keywords: ["C", "Java", "programming"]
-	},
-	{
-		title: "Graphic Design",
-		description: "I'm skilled with graphic design and digital editing using the Adobe CC, especially Photoshop and Lightroom.",
-		keywords: ["Adobe", "CC", "Photoshop", "Lightroom"]
-	}
-];
-
-var areasOfFocus = [
-	{
-		title: "Web Apps",
-		description: "Full-stack web development using JavaScript, HTML, CSS, Python, SQL, and numerous libraries/frameworks",
-		keywords: ["JavaScript", "HTML", "CSS", "Python", "SQL"]
-	},
-	{
-		title: "Desktop Apps",
-		description: "Desktop application and embedded system development using C, C++, Java, and Python",
-		keywords: ["C", "Java", "Python"]
-	},
-	{
-		title: "Graphic Design",
-		description: "Graphic design and digital editing using Adobe CC, especially Photoshop and Lightroom",
-		keywords: ["Adobe", "CC", "Photoshop", "Lightroom"]
-	}
-];
-
-var areasOfFocusOutro = "HARD-CODED DUE TO LINKS"
-
-var personalStatement = "If you are looking for an experienced and multifaceted designer and/or developer then look no further!"
-
-var pitchForVideo = "Watch this short clip right now to learn more about how I can serve YOU!"
+var personalStatement =
+  "Hey there, my name is Jon and I love building things! I consider myself to be a curious, adventurous, and motivated full-stack developer. I enjoy tinkering in my spare time, as well as biking, cooking, and MTG.";
 
 var sacState = {
-	title: "California State University, Sacramento",
-	dateRange: "2015 - 2017",
-	degree: "Computer Science, B.S.",
-	summary: "Notable coursework included parallel programming with GPUs, data structure and algorithm analysis, object-oriented programming, database management systems, and advanced computer graphics."
+  title: "California State University, Sacramento",
+  dateRange: "2015 - 2017",
+  degree: "Computer Science, B.S.",
+  summary:
+    "Notable coursework included parallel programming with GPUs, data structure and algorithm analysis, object-oriented programming, database management systems, and advanced computer graphics.",
 };
 
 var sierra = {
-	title: "Sierra College",
-	dateRange: "2011 - 2015",
-	degree: "Computer Science, A.S.",
-	summary: "Notable coursework included Java programming, C/C++ programming, graphic design, advanced mathematics, discrete structures, game design/development, and web design/development."
+  title: "Sierra College",
+  dateRange: "2011 - 2015",
+  degree: "Computer Science, A.S.",
+  summary:
+    "Notable coursework included Java programming, C/C++ programming, graphic design, advanced mathematics, discrete structures, game design/development, and web design/development.",
+};
+
+var rp = {
+  title: "Junior Software Engineer",
+  dateRange: "2019 - Present",
+  company: "Retail Pro International, LLC",
+  summary:
+    "I participate in all stages of the design and development processes and work in all areas of the product. The product is built primarily with AngularJS and Delphi and supports MySQL and Oracle (embedded).",
 };
 
 var fpc = {
-	title: "Digital Operations Associate",
-	dateRange: "2018 - 2019",
-	company: "FPC, Sacramento, CA",
-	summary: "While working for the Firearms Policy Coalition, I mainly created captivating web pages, emails, and social media posts, and I also designed graphics for those and other purposes."
+  title: "Graphic Designer",
+  dateRange: "2018 - 2019",
+  company: "Freelance",
+  summary:
+    "Through Upwork and other channels, I offered freelance graphic design with an emphasis on social media content.",
 };
 
 var irt = {
-	title: "Data Center Assistant",
-	dateRange: "2016 - 2018",
-	company: "California State University, Sacramento",
-	summary: "While working as a student assistant at Sacramento State, I not only repaired, configured, installed, and removed various data center equipment, but also wrote technical documentation, handled data backups, and performed general upkeep."
+  title: "Data Center Assistant",
+  dateRange: "2016 - 2018",
+  company: "California State University, Sacramento",
+  summary:
+    "I not only repaired, configured, installed, and removed various data center equipment, but also wrote technical documentation, handled data backups, and performed general upkeep.",
 };
-
-var proficiencies = `
-[
-    {
-        "title":"UI/UX Design","level":8,"description":"3 years experience with requirements engineering, creating wireframes and UML diagrams, and designing intuitive and aesthetic interfaces.","tags":["general"]
-    },
-    {
-        "title":"HTML5/CSS3/Bootstrap","level":8,"description":"4 years experience with developing websites using the latest iterations of HTML and CSS, often in combination with the Bootstrap framework.","tags":["general"]
-    },
-    {
-        "title":"Digital Editing & Graphic Design","level":8,"description":"3 years experience with digital editing and graphic design using Photoshop and Lightroom.","tags":["general"]
-    },
-    {
-        "title":"Communication & Teamwork","level":10,"description":"8 years experience with working efficiently and communicating effectively with team members.","tags":["general"]
-    },
-    {
-        "title":"Agile Development","level":8,"description":"2 years experience with working in a group using Agile methodologies, specifically Extreme Programming and Scrum.","tags":["general"]
-    },
-    {
-        "title":"JavaScript","level":8,"description":"3 years experience with developing full-stack web apps using Vanilla JavaScript, MongoDB, Express.js, React, Node.js, Redux, Reacstrap and more.","tags":["general"]
-    },
-    {
-        "title":"Java","level":8,"description":"4 years experience with object-oriented programming using Java for client-server applications including 2D/3D video games and much more.","tags":["general"]
-    },
-    {
-        "title":"C/C++","level":8,"description":"4 years experience with both C and C++ programming for a variety of purposes including parallel computing using GPUs.","tags":["general"]
-    },
-    {
-        "title":"SQL/MySQL","level":7,"description":"6 months experience with relational database design, MySQL, and SQL.","tags":["general"]
-    },
-    {
-        "title":"Python","level":7,"description":"1 year experience with Python programming for a variety of applications.","tags":["general"]
-    } 
-]`;
-
-/* Removed From Proficiencies
-{
-        "title":"Game Design & Development","level":6,"description":"1 year experience with designing/developing 2D and 3D Java-based video games from the ground up.","tags":["general"]
-    },
-    {
-        "title":"Unix","level":7,"description":"3 years experience with working in a Unix environment and 6 months experience with Bash scripting.","tags":["general"]
-    },
-*/
--- scripts/init-resume.js
+++ scripts/init-resume.js
@@ -1,114 +1,33 @@
-var profs_db = TAFFY(proficiencies);
-var temp_db = TAFFY();
-
-var num_of_profs = 10;
-
-/************************************************************************************************/
 fillInPersonalStatement();
-fillInSkills();
 fillInEducation();
 fillInWorkHistory();
-/************************************************************************************************/
 
 function fillInPersonalStatement() {
-	$('.old-personal-statement-2').text(oldPersonalStatement2);
-	$('.areas-of-focus-intro').text(areasOfFocusIntro);
-	//$('.personal-statement').text(personalStatement);
-	//$('.pitch-for-video').text(pitchForVideo);
-	//$('.areas-of-focus-1-title').text(areasOfFocus[0].title);
-	//$('.areas-of-focus-2-title').text(areasOfFocus[1].title);
-	//$('.areas-of-focus-3-title').text(areasOfFocus[2].title);
-	$('.areas-of-focus-1-description').append(boldKeywords(areasOfFocus[0].description, areasOfFocus[0].keywords));
-	$('.areas-of-focus-2-description').append(boldKeywords(areasOfFocus[1].description, areasOfFocus[1].keywords));
-	$('.areas-of-focus-3-description').append(boldKeywords(areasOfFocus[2].description, areasOfFocus[2].keywords));
-	$('.areas-of-focus-outro').text(areasOfFocusOutro);
-}
-
-function boldKeywords(rawText, listOfKeywords) {
-	/*
-	var regExpStr = "\\";
-	listOfKeywords.map((kw, index, array) => {
-		if(index < array.length - 1) regExpStr = regExpStr.concat(kw + "\\|\\");
-		else regExpStr = regExpStr.concat(kw + "\\");
-	});
-	//regExpStr = regExpStr.concat(")");
-	var regExp = new RegExp("(" + regExpStr + ")", 'g');
-	return ("<p>" + rawText.replace(regExp, '<b>$1</b>') + "</p>");
-	//return preg_replace('/\b('.implode('|', listOfKeywords).')\b/i', '<b>$1</b>', rawText);
-	*/
-	var regExp = new RegExp("(" + listOfKeywords.join("|") + ")", 'g');
-	return "<p>" + rawText.replace(regExp, "<b>$1</b>") + "</p>";
+  $(".personal-statement").text(personalStatement);
 }
 
 function fillInEducation() {
-	$('.sac-state-title').text(sacState.title);
-	$('.sac-state-date-range').text(sacState.dateRange);
-	$('.sac-state-degree').text(sacState.degree);
-	$('.sac-state-summary').text(sacState.summary);
-	$('.sierra-title').text(sierra.title);
-	$('.sierra-date-range').text(sierra.dateRange);
-	$('.sierra-degree').text(sierra.degree);
-	$('.sierra-summary').text(sierra.summary);
+  $(".sac-state-title").text(sacState.title);
+  $(".sac-state-date-range").text(sacState.dateRange);
+  $(".sac-state-degree").text(sacState.degree);
+  $(".sac-state-summary").text(sacState.summary);
+  $(".sierra-title").text(sierra.title);
+  $(".sierra-date-range").text(sierra.dateRange);
+  $(".sierra-degree").text(sierra.degree);
+  $(".sierra-summary").text(sierra.summary);
 }
 
 function fillInWorkHistory() {
-	$('.fpc-title').text(fpc.title);
-	$('.fpc-date-range').text(fpc.dateRange);
-	$('.fpc-company').text(fpc.company);
-	$('.fpc-summary').text(fpc.summary);
-	$('.irt-title').text(irt.title);
-	$('.irt-date-range').text(irt.dateRange);
-	$('.irt-company').text(irt.company);
-	$('.irt-summary').text(irt.summary);
-}
-
-function fillInSkills() {
-	populateTempDb('general');
-	updateProfs();
-}
-
-function populateTempDb(c1, c2, c3, c4, c5) {
-    temp_db().remove();
-    profs_db({tags:{has: [c1,c2, c3, c4, c5]}}).each(function (r) {
-        temp_db.insert(r);
-    });
-    temp_db.sort("level");
-}
-
-function updateProfs() {
-    var i = num_of_profs;
-    temp_db().each(function (r) {
-        temp_db(r).update({id:i});
-        i--;
-    });
-
-    var profCounter;
-    for(profCounter = 1; profCounter < num_of_profs + 1; profCounter++) {
-        var title = "prof-" + profCounter + "-title";
-        var level = "prof-" + profCounter + "-shape";
-        var desc = "prof-" + profCounter + "-description";
-        var titles = document.getElementsByClassName(title);
-
-        var i;
-        for (i = 0; i < titles.length; i++) {
-            titles[i].innerHTML = temp_db({id:profCounter}).first().title;
-        }
-        var progressBarShapes = document.getElementsByClassName(level);
-        var j = temp_db({id:profCounter}).first().level;
-        for (i = 0; i < j; i++) {
-            progressBarShapes[i].style.backgroundColor = "#ffffff";
-        }
-        for(i = 10; i < j + 10; i++) {
-            progressBarShapes[i].style.backgroundColor = "#ffffff";
-        }
-        document.getElementById(desc).innerHTML = temp_db({id:profCounter}).first().description;
-    }
-}
-
-function toggle() {
-    if ($("#skills-more-less").attr('aria-expanded') === "true") {
-        $("#skills-more-less").text('Show More');
-    } else {
-        $("#skills-more-less").text('Show Less');
-    }
+  $(".rp-title").text(rp.title);
+  $(".rp-date-range").text(rp.dateRange);
+  $(".rp-company").text(rp.company);
+  $(".rp-summary").text(rp.summary);
+  $(".fpc-title").text(fpc.title);
+  $(".fpc-date-range").text(fpc.dateRange);
+  $(".fpc-company").text(fpc.company);
+  $(".fpc-summary").text(fpc.summary);
+  $(".irt-title").text(irt.title);
+  $(".irt-date-range").text(irt.dateRange);
+  $(".irt-company").text(irt.company);
+  $(".irt-summary").text(irt.summary);
 }
