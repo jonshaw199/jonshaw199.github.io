7327bf94f0d7c3695e36a9b40565b5a7d8baaf12
Jon <jonshaw199@gmail.com>
2022-06-21 18:52:04 -0700

    Adding job links

--- js/data.js
+++ js/data.js
@@ -11,30 +11,35 @@ const workExperience = [
     "Data Center Technician (Student Assistant)",
     "Jul 2016 - Mar 2018",
     "Installed, configured, and repaired data center equipment. Wrote technical documentation, handled data backups, and performed basic upkeep of the facility.",
+    "https://www.csus.edu/information-resources-technology/networking-infrastructure/data-center-administration.html",
   ],
   [
     "Retail Pro International",
     "Software Development Support",
     "Apr 2019 - Sep 2020",
     "",
+    "https://www.retailpro.com/",
   ],
   [
     "Retail Pro International",
     "Junior Software Engineer",
     "Sep 2020 - Aug 2021",
     "Actively participated in Prism POS full-stack development, building powerful features like Auto-Replenishment as well as refining existing functionality like Advanced Item Search. Handled database administration tasks such as modeling, scripting, and tuning. Practiced test-driven development and continuous integration, working with QA on a daily basis.",
+    "https://www.retailpro.com/",
   ],
   [
     "Retail Pro International",
     "Web Developer",
     "Aug 2021 - Jan 2022",
     "Maintained and enhanced the Retail Pro Ordering System which is used to facilitate all product and software assurance orders by clients and partners. Modernized the retailpro.com marketing site and enhanced the mobile experience. Improved the my.retailpro.com internal site and implemented additional security measures.",
+    "https://www.retailpro.com/",
   ],
   [
     "Rhombus Systems",
     "Frontend Engineer",
     "Jan 2022 - Present",
     "Building new Rhombus Console features and maintaining existing ones. Developing for both web and mobile, including iOS and Android. Writing comprehensive unit and integration tests. Participating in UI/UX design.",
+    "https://www.rhombussystems.com/",
   ],
 ];
 
